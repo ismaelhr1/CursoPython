d847d07def929270f9c1a3d2b7b9fd8a8d98b02e
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-01-14 11:49:47 +0100

    feat: Ejercicio 2

---
+++ Prueba/Ejercicio2.py
@@ -0,0 +1,12 @@
+print("-------------")
+print("MEDIA DE NOTAS DEL ALUMNO")
+print("-------------")
+
+print("Ingrese las notas del alumno")
+N1=float(input("Nota 1: "))
+N2=float(input("Nota 2: "))
+N3=float(input("Nota 3: "))
+
+P=(N1+N2+N3)/3
+
+print("MEDIA: ",P)
