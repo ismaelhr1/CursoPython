b1781610af63a6574a6cc335c0b7f66c4e76bc60
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-01-14 11:15:01 +0100

    feat: Ejercicio1

---
+++ Prueba/Ejercicio1.py
@@ -0,0 +1,16 @@
+#Decoración nombre del algoritmo 
+print("----------------")
+print("Ejercicio 1: CALCULAR DISTANCIA")
+print("----------------")
+
+#Entradas
+print("Ingrese la velocidad y el tiempo de la unidad móvil")
+#Convirtiendo entrada a Entero
+V=float(input("Velocidad: "))
+T=int(input("Tiempo: "))
+
+#Proceso
+D=V*T
+
+#Salida
+print("Distancia recorrida: ",D)
