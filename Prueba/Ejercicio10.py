3de344dfff6b39022bffc3fe0d60907b44a7000f
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-01-18 16:58:26 +0100

    feat:Ejercicio10

---
+++ Prueba/Ejercicio10.py
@@ -0,0 +1,14 @@
+print("--------------")
+print("DATOS DEL CILINDRO")
+print("--------------")
+
+RADIO=float(input("Introducir RADIO DEL CILINDRO: "))
+ALTO=float(input("Introducir ALTO DEL CILINDRO: "))
+
+PI=3.14
+
+VOL=PI*(RADIO**2)*ALTO
+ARE=(2*PI)*RADIO*(ALTO+RADIO)
+
+print("VOLUMEN: ", VOL)
+print("AREA: ", ARE)
