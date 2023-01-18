798c0d712dccb19fab16b16aae44422b78e4d3ed
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-01-18 16:30:57 +0100

    feat:Ejercicio8

---
+++ Prueba/Ejercicio8.py
@@ -0,0 +1,13 @@
+print("---------")
+print("RECTÁNGULO")
+print("---------")
+
+BASE=float(input("Introduce la medida de la Base: "))
+ALTO=float(input("Introduce la medida del Alto: "))
+
+SUP=BASE*ALTO
+PER=2*(BASE+ALTO)
+
+print("Superficie: ", SUP)
+print("Perímetro: ", PER)
+
