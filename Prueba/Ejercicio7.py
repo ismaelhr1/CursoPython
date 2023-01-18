fdef8f2d4fbe4bf87392dafdcb11994902816cda
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-01-18 16:17:26 +0100

    feat:Ejercicio7

---
+++ Prueba/Ejercicio7.py
@@ -0,0 +1,12 @@
+print("-------------------")
+print("PROMEDIO DE DOS NUMEROS")
+print("-------------------")
+
+N1=float(input("Indique el N1: "))
+N2=float(input("Indique el N2: "))
+
+S=N1+N2
+P=S/2
+
+print("Promedio: ", S)
+
