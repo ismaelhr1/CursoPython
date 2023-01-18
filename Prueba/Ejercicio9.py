5830f507bd04a158ecb8ee93454ef10d8e51b38e
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-01-18 16:44:34 +0100

    feat:Ejercicio9

---
+++ Prueba/Ejercicio9.py
@@ -0,0 +1,13 @@
+print("----------------")
+print("GASOLINERA")
+print("----------------")
+
+CONSU=float(input("Introduzca los litros surtidos: "))
+
+LITXG=3.785
+PRECIOXL=4.50
+
+TOTAL=CONSU*LITXG*PRECIOXL
+
+print("PRECIO TOTAL: ", TOTAL)
+
