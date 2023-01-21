226d4aaff1da70a82bdb0b806424109b1de451c1
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-01-21 14:01:09 +0100

    feat:Complementario5

---
+++ Prueba/Complementario5.py
@@ -0,0 +1,10 @@
+print("-----------")
+print("PRECIO ARTÍCULO")
+print("-----------")
+
+C=float(input("Ingrese el costo unitario del artículo: "))
+D=int(input("Ingrese el número de docenas: "))
+
+P=D*12*C
+
+print("El precio del artículo es: ",P)
