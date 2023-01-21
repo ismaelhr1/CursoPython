2497ddbe72a08e33df5fa5b74c094091d840989d
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-01-21 13:07:20 +0100

    feat:Complementario1

---
+++ Prueba/Complementario1.py
@@ -0,0 +1,11 @@
+print("----------------")
+print("VOLUMEN ESFERA")
+print("----------------")
+
+PI=3.14
+
+R=float(input("Ingrese el radio: "))
+
+V=4/3*PI*R**3
+
+print("El volumen de la esfera es: ", V)
