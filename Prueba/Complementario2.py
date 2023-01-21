db59341baff61cc2ce26aecc46908dc65d29a712
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-01-21 13:18:23 +0100

    feat:Complementario2

---
+++ Prueba/Complementario2.py
@@ -0,0 +1,10 @@
+print("-----------")
+print("PERÍMETRO TRIÁNGULO EQUILATERO")
+print("-----------")
+
+print("Ingrese la altura del triángulo: ")
+H=float(input())
+
+P=3*(2*H/3**0.5)
+
+print("El perímetro del triángulo será: ", P)
