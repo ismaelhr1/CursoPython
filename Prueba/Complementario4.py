92e3e5f47dd1059bc9949681316e0e27f63230e7
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-01-21 13:42:48 +0100

    feat:Complementario4

---
+++ Prueba/Complementario4.py
@@ -0,0 +1,11 @@
+print("-----------")
+print("VELOCIDAD MÓVIL")
+print("-----------")
+
+T=float(input("Ingrese el tiempo en segundos: "))
+D=float(input("Ingresa la distancia en metros: "))
+
+V=D/T
+
+print("La velocidad es: ",V, "m/s")
+
