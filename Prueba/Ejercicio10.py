60ee414779f6fd0a855a13c55b48961e832eee8b
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-01-21 13:46:58 +0100

    refactor:Ejercicio10

--- Prueba/Ejercicio10.py
+++ Prueba/Ejercicio10.py
@@ -5,7 +5,7 @@ print("--------------")
 RADIO=float(input("Introducir RADIO DEL CILINDRO: "))
 ALTO=float(input("Introducir ALTO DEL CILINDRO: "))
 
-PI=3.14
+PI=3.1416
 
 VOL=PI*(RADIO**2)*ALTO
 ARE=(2*PI)*RADIO*(ALTO+RADIO)
