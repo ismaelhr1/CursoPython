9bf599db7b77571dfbf32c5aa4585bfdeafa7fd1
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-01-21 13:30:35 +0100

    feat:Complementario3

---
+++ Prueba/Complementario3.py
@@ -0,0 +1,15 @@
+print("-----------")
+print("SOLES EN EUROS Y DOLARES")
+print("-----------")
+
+EU=3.84
+DO=2.82
+
+S=float(input("Ingrese la cantidad de Soles: "))
+
+D=S/DO
+E=S/EU
+
+print("En", S, "soles hay", E, "euros.")
+print("En", S, "soles hay", D, "dolares.")
+
