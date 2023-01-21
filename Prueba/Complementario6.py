8d2b7bc50bc03a403b2ecb375c97970853bc4eee
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-01-21 14:25:48 +0100

    feat:Complementario6

---
+++ Prueba/Complementario6.py
@@ -0,0 +1,12 @@
+print("-----------")
+print("CONVERTIDOR MILLAS A KILOMETROS")
+print("-----------")
+
+MI=1.609344
+
+M=float(input("Ingrese número de Millas: "))
+
+K=M*MI
+
+print(M,"millas tiene",K,"kilómetros")
+
