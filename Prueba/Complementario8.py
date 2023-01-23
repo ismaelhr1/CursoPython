bd8b8e68ee8d6a9507a016bf637058311ffd8475
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-01-23 21:23:51 +0100

    feat:Complementario8

---
+++ Prueba/Complementario8.py
@@ -0,0 +1,11 @@
+print("-----------")
+print("TIEMPO DE ENCUENTRO")
+print("-----------")
+
+VA=float(input("Ingrese la velocidad del cuerpo A: "))
+VB=float(input("Ingrese la velocidad del cuerpo B: "))
+D=float(input("Ingrese la distancia que los separa: "))
+
+TE=D/(VA+VB)
+
+print("Los cuerpos se encontraran en",TE,"segundos")
