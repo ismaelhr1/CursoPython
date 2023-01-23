c1e857c9f118b4f5a44fc9ff339a3ea1ef09a491
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-01-23 21:08:13 +0100

    feat:ComplementarioExtra

---
+++ Prueba/ComplementarioExtra.py
@@ -0,0 +1,10 @@
+print("------------")
+print("CREAR DIRECTORIO EN ESCRITORIO")
+print("------------")
+
+import os
+
+#dir1 = "C:\\Users\\User\\Desktop\\PruebaDir"   
+#dir2 = "..\..\\PruebaDir"   
+#os.mkdir(dir2)
+#os.rmdir("dir2") 
