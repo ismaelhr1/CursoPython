0bae7d833b52183ebf73a24e415ece90a7767ad1
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-01-25 19:33:25 +0100

    feat:Complementario9

---
+++ Prueba/Complementario9.py
@@ -0,0 +1,14 @@
+print("-----------")
+print("CONVERSIÓN SEXAGESIMAL Y CENTESIMAL")
+print("-----------")
+
+PI=3.1416
+
+X=float(input("Ingrese ángulo en Radianes: "))
+
+SEX=X*180/PI
+CEN=X*200/PI
+
+print("En sexagesimales es: ",SEX)
+print("En centesimales es: ",CEN)
+
