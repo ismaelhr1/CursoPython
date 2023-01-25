667816b22e6d9c1560b04a08240476a8f24e2716
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-01-25 20:13:35 +0100

    feat:EstructuraSelectivaDoble

---
+++ Prueba/EstructuraSelectivaDoble.py
@@ -0,0 +1,13 @@
+print("-----------")
+print("EVALUAR EL MENOR NÚMERO")
+print("-----------")
+
+print("Ingrese 2 números")
+X=int(input("Primer número: "))
+Y=int(input("Segundo número: "))
+
+if X>Y:
+    print("El menor es ",Y)
+else:
+    print("El menos es:",X)
+
