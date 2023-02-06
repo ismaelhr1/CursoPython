8d75257d2923759023b6b12166f9adf4bf536e5a
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-02-06 20:16:02 +0100

    feat:Ejercicio11

---
+++ Prueba/Ejercicio11.py
@@ -0,0 +1,10 @@
+print("-----------")
+print("VERIFICA SI EL ALUMNO ESTA APROBADO")
+print("-----------")
+
+print("Ingrese la nota")
+
+CAL=int(input("Nota del alumno: "))
+
+if CAL>10:
+    print("APROBADO")
