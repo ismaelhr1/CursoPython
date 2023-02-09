64e5c52441f36e8902415de57a7173808b35bffd
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-02-09 20:47:27 +0100

    feat:Complementario4_1

---
+++ Prueba/Complementario4_1.py
@@ -0,0 +1,14 @@
+print("-----------")
+print("EJERCICIO IGUALES")
+print("-----------")
+
+A=int(input("Primer número: "))
+B=int(input("Segundo número: "))
+C=int(input("Tercer número: "))
+
+if A+B==C:
+    print("IGUALES")
+else:
+        print("DISTINTOS")
+
+        
