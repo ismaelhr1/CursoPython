4e87b6f6a3353cc20535017fb9ae9b1ceecc74e6
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2023-02-09 20:54:15 +0100

    feat:Complementario4_2

---
+++ Prueba/Complementario4_2.py
@@ -0,0 +1,13 @@
+print("-----------")
+print("MENOR A MAYOR")
+print("-----------")
+
+A=int(input("Primer número: "))
+B=int(input("Segundo número: "))
+
+if A<B:
+    print("Los números son: ",A, B)
+else:
+    print("Los números son: ",B, A)
+
+    
